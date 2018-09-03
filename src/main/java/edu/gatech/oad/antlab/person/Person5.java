860bd64320367aad23d6b123bcc672356e90f084
jdrake38 <jdrake38@gatech.edu>
2018-09-03 12:32:00 -0400

    Completed person5.java

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,9 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		String firstTwo = input.substring(0,2);
+		String remaining = input.substring(2, input.length() - 1);
+	  return remaining + firstTwo;
 	}
 	
 	/**
