64ab5f397f5eee448b50b1d12223d9e3ecab5be3
jdrake38 <jdrake38@gatech.edu>
2018-09-04 19:11:10 -0400

    Added name and email to AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("epao3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Jaewon");
+                  toPrint += p5.toString("903254006");
 		  
         System.out.println(toPrint);
         
