530634c712b02141e323d921f7dc568a96ea03ad
Philip Glover <glover.philip@gmail.com>
2018-09-16 11:28:11 -0400

    Real initial commit

--- app/src/main/java/com/example/notphilphil/bob/controllers/MainActivity.java
+++ app/src/main/java/com/example/notphilphil/bob/controllers/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.notphilphil.bob.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.notphilphil.bob.R;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+    private TextView main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_main);
+
+        main = (TextView) findViewById(R.id.main);
     }
 }
