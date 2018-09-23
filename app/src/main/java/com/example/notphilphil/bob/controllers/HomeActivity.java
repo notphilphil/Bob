a1a94cba0ff7782da365db410ddcd148303ba311
Philip Glover <glover.philip@gmail.com>
2018-09-23 16:31:29 -0400

    Add untracked files

---
+++ app/src/main/java/com/example/notphilphil/bob/controllers/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.notphilphil.bob.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.notphilphil.bob.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
