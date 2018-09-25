96f6d8d6955137838ccf03e293abfcd9d3043fe2
Philip Glover <glover.philip@gmail.com>
2018-09-25 13:36:18 -0400

    Add back to login functionality

--- app/src/main/java/com/example/notphilphil/bob/controllers/RegisterActivity.java
+++ app/src/main/java/com/example/notphilphil/bob/controllers/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.notphilphil.bob.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.notphilphil.bob.R;
 
@@ -11,5 +14,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button back_to_login_bt = findViewById(R.id.login_button);
+        back_to_login_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
