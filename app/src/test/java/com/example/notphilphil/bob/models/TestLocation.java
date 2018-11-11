71dda6f852daf57767700012eb020d05c969d971
Jaewon Drake <jpdrake@jaewons-mbp.attlocal.net>
2018-11-11 16:53:11 -0500

    Fixed test names to match Java naming convention

--- app/src/test/java/com/example/notphilphil/bob/models/TestLocation.java
+++ app/src/test/java/com/example/notphilphil/bob/models/TestLocation.java
@@ -10,61 +10,61 @@ public class TestLocation {
     public static final Location testing = new Location();
 
     @Test
-    public void AddLatitude() {
+    public void addLatitude() {
         testing.addValue(Location.tokens[0], testTokens[0]);
         assertEquals(testTokens[0], Double.toString(testing.getLatitude()));
     }
 
     @Test
-    public void AddLongitude() {
+    public void addLongitude() {
         testing.addValue(Location.tokens[1], testTokens[1]);
         assertEquals(testTokens[1], Double.toString(testing.getLongitude()));
     }
 
     @Test
-    public void AddStreetAddress() {
+    public void addStreetAddress() {
         testing.addValue(Location.tokens[2], testTokens[2]);
         assertEquals(testTokens[2], testing.getStreetAddress());
     }
 
     @Test
-    public void AddCity() {
+    public void addCity() {
         testing.addValue(Location.tokens[3], testTokens[3]);
         assertEquals(testTokens[3], testing.getCity());
     }
 
     @Test
-    public void AddState() {
+    public void addState() {
         testing.addValue(Location.tokens[4], testTokens[4]);
         assertEquals(testTokens[4], testing.getState());
     }
 
     @Test
-    public void AddZip() {
+    public void addZip() {
         testing.addValue(Location.tokens[5], testTokens[5]);
         assertEquals(testTokens[5], testing.getZip());
     }
 
     @Test
-    public void AddType() {
+    public void addType() {
         testing.addValue(Location.tokens[6], testTokens[6]);
         assertEquals(testTokens[6], testing.getType());
     }
 
     @Test
-    public void AddPhone() {
+    public void addPhone() {
         testing.addValue(Location.tokens[7], testTokens[7]);
         assertEquals(testTokens[7], testing.getPhone());
     }
 
     @Test
-    public void AddWebsite() {
+    public void addWebsite() {
         testing.addValue(Location.tokens[8], testTokens[8]);
         assertEquals(testTokens[8], testing.getWebsite());
     }
 
     @Test
-    public void AddName() {
+    public void addName() {
         testing.addValue(Location.tokens[9], testTokens[9]);
         assertEquals(testTokens[9], testing.getName());
     }
