f649ace972a20a7e29a06b60641ef050a2717be5
Spencer Huang <sirsh8@gmail.com>
2018-11-13 15:12:44 -0500

    add testloginactivity

---
+++ app/src/test/java/com/example/notphilphil/bob/controllers/TestLoginActivity.java
@@ -0,0 +1,61 @@
+package com.example.notphilphil.bob.controllers;
+
+
+import android.content.Context;
+import android.test.mock.MockContext;
+
+import com.example.notphilphil.bob.controllers.LoginActivity;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class TestLoginActivity {
+    static File file;
+
+    @BeforeAll
+    public static void buildFile() {
+        LoggedUser.setTesting(true);
+        file = new File("regUsers.txt");
+    }
+
+    private void addUser(String name, String email, String pass, LoggedUser.PermissionsEnum usertype) {
+        try {
+            OutputStreamWriter outWriter =
+                    new OutputStreamWriter(new FileOutputStream(file, true));
+            String toWrite = name+","+email+","+pass+","+usertype+"\n";
+            outWriter.write(toWrite);
+            outWriter.close();
+        } catch (IOException err) {
+
+        }
+    }
+
+    @Test
+    public void testLogin() {
+        LoginActivity testClass = new LoginActivity();
+        String user = "phil3";
+        String pass = "lip3";
+        assertFalse(testClass.loginPressed(user, pass, file, new MockContext()));
+    }
+
+    @Test
+    public void testLoginUserExists() {
+        LoginActivity testClass = new LoginActivity();
+        String user = "phil";
+        String email = "pass";
+        String pass = "lip";
+        LoggedUser.PermissionsEnum user_type = LoggedUser.PermissionsEnum.USER;
+        addUser(user, email, pass, user_type);
+        assertTrue(testClass.loginPressed(user, pass, file, new MockContext()));
+    }
+}
